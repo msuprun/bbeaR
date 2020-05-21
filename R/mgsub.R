57609ccae7ea467028bc25d7af1ee17e38d487e9
msuprun <suprun.maria@gmail.com>
2020-05-21 10:33:57 -0400

    man 2

---
+++ R/mgsub.R
@@ -0,0 +1,11 @@
+mgsub<-function (pattern, replacement, x, ...) 
+{
+  if (length(pattern) != length(replacement)) {
+    stop("pattern and replacement do not have the same length.")
+  }
+  result <- x
+  for (i in 1:length(pattern)) {
+    result <- gsub(pattern[i], replacement[i], result, ...)
+  }
+  result
+}
